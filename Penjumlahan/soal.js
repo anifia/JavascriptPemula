af8ab8bb112f7a6d9fe59ebca26c12c12e847171
Haniifah Tsaabitah Filhaq <91241519+anifia@users.noreply.github.com>
2021-12-31 20:28:57 +0700

    Add files via upload

---
+++ Penjumlahan/soal.js
@@ -0,0 +1,6 @@
+function penjumlahan(){
+    var angka1 = parseInt(document.getElementById("angka1").value);
+    var angka2 = parseInt(document.getElementById("angka2").value);
+
+    document.getElementById("hasil").innerHTML = angka1 + angka2
+}
